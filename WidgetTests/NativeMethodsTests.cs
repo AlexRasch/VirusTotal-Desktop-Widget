349dee32fcb88feb114f1309cd0827b1632f16ff
AlexRasch <alexander.rasch@malardalenvvs.se>
2023-07-12 13:11:10 +0200

    Started on tests for Fade in & out

---
+++ WidgetTests/NativeMethodsTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WidgetTests
+{
+    public class NativeMethodsTests
+    {
+        [Fact]
+        public void FadeIn_ChangesWindowOpacity()
+        {
+
+        }
+
+        [Fact]
+        public void FadeOut_ChangesWindowOpacity()
+        {
+
+        }
+    }
+}
