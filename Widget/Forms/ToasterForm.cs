3b457996db5e4f8c132e830fbd8191ef492ac546
AlexRasch <alexander.rasch@malardalenvvs.se>
2023-07-16 00:54:33 +0200

    Added comments to Toaster public functions

--- Widget/Forms/ToasterForm.cs
+++ Widget/Forms/ToasterForm.cs
@@ -7,7 +7,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.ComponentModel.DataAnnotations;
 using System.Diagnostics;
 
 namespace Widget.Forms
@@ -15,14 +14,31 @@ namespace Widget.Forms
 #pragma warning disable IDE1006
     public partial class ToasterForm : Form
     {
-        [Required(ErrorMessage = "Title is required.")]
+        /// <summary>
+        /// Gets or sets the title of the toaster form.
+        /// </summary>
         public string Title { get; set; }
-        [Required(ErrorMessage = "Message is required.")]
+        /// <summary>
+        /// Gets or sets the message of the toaster form.
+        /// </summary>
         public string Message { get; set; }
+        /// <summary>
+        /// Gets or sets the display duration of the toaster form.
+        /// </summary>
         public int? DisplayDuration { get; set; } = 5000;
 
         private readonly System.Windows.Forms.Timer displayTimer;
 
+        /// <summary>
+        /// Creates a new toaster form and displays it.
+        /// </summary>
+        /// <param name="title">The title for the toaster form.</param>
+        /// <param name="message">The message for the toaster form.</param>
+        /// <param name="displayDuration">The duration (in milliseconds) to display the toaster form.</param>
+        /// <remarks>
+        /// This constructor is used to create a toaster form with the specified title, message, and display duration.
+        /// Once created, the toaster form will be displayed for the specified duration and then automatically closed.
+        /// </remarks>
         public ToasterForm(string title, string message, int displayDuration)
         {
             this.Title = title;
