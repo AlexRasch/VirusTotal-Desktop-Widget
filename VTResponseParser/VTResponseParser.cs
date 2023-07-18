7e09abe7397c83ac0983486b54a28d17b5e01783
AlexRasch <alexander.rasch@malardalenvvs.se>
2023-07-18 04:48:38 +0200

    Moved the props to a seperate file and made the parser class partial

--- VTResponseParser/VTResponseParser.cs
+++ VTResponseParser/VTResponseParser.cs
@@ -8,64 +8,8 @@ namespace VirusTotal
     // It is recommended to refer to the official VirusTotal API documentation for a comprehensive implementation.
     // Instead of using tools like json2csharp.com, I manually created the classes.
 
-    public class ResponseParser
+    public partial class ResponseParser
     {
-        public ResponseParser.Error? ErrorCode { get; set; }
-        public Meta.FileInfo FileInfo { get; set; }
-        /// <summary>
-        /// Represents the links associated with the VirusTotal API response.
-        /// </summary>
-        public LinksResponse Links { get; set; }
-        public string Status { get; set; }
-        public string Id { get; set; }
-        public string Type { get; set; }
-        public Dictionary<string, EngineResult> Results { get; set; } // New property for results
-        public class Error
-        {
-            public string? Message { get; set; }
-            public string? Code { get; set; }
-        }
-        public struct Meta
-        {
-            public struct FileInfo
-            {
-                public string SHA256 { get; set; }
-                public string SHA1 { get; set; }
-                public string MD5 { get; set; }
-                public int Size { get; set; }
-            }
-        }
-        public class EngineResult
-        {
-            public string? Category { get; set; }
-            public string? EngineName { get; set; }
-            public string? EngineVersion { get; set; }
-            public string? Result { get; set; }
-            public string? Method { get; set; }
-            public string? EngineUpdate { get; set; }
-        }
-
-        /// <summary>
-        /// Represents the links associated with the VirusTotal API response.
-        /// </summary>
-        public struct LinksResponse
-        {
-            /// <summary>
-            /// Gets or sets the link to the item related to the analysis.
-            /// </summary>
-            public string Item { get; set; }
-            /// <summary>
-            /// Gets or sets the link to the self-analysis result.
-            /// </summary>
-            public string Self { get; set; }
-        }
-        /// <summary>
-        /// Indicates whether the response parsing is complete.
-        /// Note: This property is not part of the VirusTotal API.
-        /// </summary>
-        public bool IsComplete { get; set; }
-
-
         public ResponseParser ParseReport(string? responseContent)
         {
             var report = new ResponseParser();
