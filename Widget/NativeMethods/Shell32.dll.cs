bfd3b4442eac963b3aad7e82171097a00f1f8fc7
AlexRasch <alexander.rasch@malardalenvvs.se>
2023-08-17 17:38:17 +0200

    refactor should merge before we continue

--- Widget/NativeMethods/Shell32.dll.cs
+++ Widget/NativeMethods/Shell32.dll.cs
@@ -16,6 +16,12 @@ namespace Widget
             public struct UserNotificationState
             {
                 public int State;
+                /// <summary>
+                /// A screen saver is displayed, the machine is locked, or a nonactive Fast User Switching session is in progress.
+                /// </summary>
+                public bool QUNS_NOT_PRESENT => State == 0;
+
+                public bool IsFullScreenRunning => State == 3;
             }
 
             [DllImport("shell32.dll")]
